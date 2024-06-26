bbf5c59cb9a3b8ace0d9fd0b2d041a8fa15cc47d
Tarannum Binte Tariq <106380665+twinkletariq@users.noreply.github.com>
2024-06-26 19:37:28 +0600

    Add files via upload

---
+++ PIng pong ball/src/ping/pong/ball/PongGame.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ping.pong.ball;
+
+/**
+ *
+ * @author HP
+ */
+public class PongGame {
+
+	public static void main(String[] args) {
+		
+		GameFrame frame = new GameFrame();
+		
+	}
+}
